2467c4760e54dddc89c68b9d60561abbeefa8540
PeAriel <82453690+PeAriel@users.noreply.github.com>
2021-10-19 21:57:49 +0300

    Create help_messages.py

---
+++ help_messages.py
@@ -0,0 +1,13 @@
+
+perspectiveHelp = "By default, FEN readers assumes the position is viewed from whit's perspective.\n" +\
+                  "If in the snippet you took the position was viewd from black's perspective, please\n " +\
+                   "adjust this field."
+
+castelingHelp = 'These buttons specifies if casteling is still a possibility.\n' +\
+                'Uncheck both O-O and O-O-O if a player has already casteled.'
+
+enPassanthelp = ''
+
+halfMoveHelp = ''
+
+fullMoveHelp = ''
